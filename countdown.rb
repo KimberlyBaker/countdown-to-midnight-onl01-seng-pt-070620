26624b250440e4b07c6b6ee560cd9e6b99592100
Kimberly Baker <bakerk2010@gmail.com>
2020-07-19 23:55:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,4 @@
 def countdown(number)
-  number = 10
   until number == 0
     puts " #{number} SECOND(S)!"
     number -= 1
@@ -13,7 +12,6 @@ countdown
 
 
 def countdown_with_sleep(number)
-  number = 10
   until number == 0
     puts " #{number} SECOND(S)!"
     number -= 1
