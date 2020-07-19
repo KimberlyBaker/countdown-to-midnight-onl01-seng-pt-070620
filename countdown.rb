3b341eb4c2d190b2a7659838ce0840f48e00dc96
Kimberly Baker <bakerk2010@gmail.com>
2020-07-19 23:47:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,13 @@
-def countdown(number)
+def countdown_with_sleep
+  number = 10
   until number == 0
-    puts "#{number} SECOND(S)!"
+    puts " #{number} SECOND(S)!"
     number -= 1
-    sleep 0.5
+    sleep 1
   end
 
 puts "HAPPY NEW YEAR!"
 
 end
+
+countdown_with_sleep
