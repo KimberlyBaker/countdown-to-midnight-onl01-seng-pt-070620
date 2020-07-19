617414834b04069b07744cf7fb15ba763ca4904a
Kimberly Baker <bakerk2010@gmail.com>
2020-07-19 23:49:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,18 @@
+def countdown
+  number = 10
+  until number == 0
+    puts " #{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+
+puts "HAPPY NEW YEAR!"
+
+end
+
+countdown_with_sleep
+
+
 def countdown_with_sleep
   number = 10
   until number == 0
